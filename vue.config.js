2e7e86e2cd311d165419a41a11563ebbfb2ff07a
Kevin Tosli <kevintosli@icloud.com>
2020-01-16 17:31:43 -0300

    Added src/scss to aliases

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,7 @@ module.exports = {
         "@gn-icons": path.resolve(__dirname, "packages/icons"),
         "@components": path.resolve(__dirname, "src/components"),
         "@assets": path.resolve(__dirname, "src/assets"),
+        "@scss": path.resolve(__dirname, "src/scss"),
         "@js": path.resolve(__dirname, "src/js"),
         "@router": path.resolve(__dirname, "src/router"),
         "@store": path.resolve(__dirname, "src/store"),
